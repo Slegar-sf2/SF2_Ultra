d357c8116b0385fae2b183f55f36bb22d9e14a55
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-07-31 14:28:25 +0300

    SF2_Ultra
    желз из карты 3 -19 перемещение

--- disasm/data/maps/entries/map03/7-chest-items.asm
+++ disasm/data/maps/entries/map03/7-chest-items.asm
@@ -1,6 +1,5 @@
 
 ; ASM FILE data\maps\entries\map03\7-chest-items.asm :
 ; 0x9793A..0x97940 : 
-                mapItem 6, 18, 220, NOTHING
-				mapItem 46, 9, 140, SHORT_ROD
+                mapItem 6, 18, 220, NOTHING				
                 endWord
--- disasm/data/maps/entries/map19/8-other-items.asm
+++ disasm/data/maps/entries/map19/8-other-items.asm
@@ -1,4 +1,5 @@
 
 ; ASM FILE data\maps\entries\map19\8-other-items.asm :
-; 0xA4696..0xA4698 : 
+; 0xA4696..0xA4698 :
+                mapItem 18, 17, 140, SHORT_ROD
                 endWord
