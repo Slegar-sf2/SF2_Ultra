65bbc36eed88018430aa5b4b2d5a06c93169771e
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-08-03 06:36:08 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map87/7-chest-items.asm
+++ disasm/data/maps/entries/map87/7-chest-items.asm
@@ -1,7 +1,6 @@
 
                 mapItem 9, 3, 230, MANA
-				mapItem 18, 7, 231, HEALING_WATER
-				mapItem 19, 7, 232, MITHRIL
-				mapItem 25, 4, 233, HEALING_SEED
+                mapItem 18, 7, 231, HEALING_WATER
+                mapItem 19, 7, 232, MITHRIL
+                mapItem 25, 4, 233, HEALING_SEED
                 endWord
-                
