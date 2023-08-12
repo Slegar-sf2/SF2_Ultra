00fb31b6a8fa486403e71baceb3f2af969ddfbdb
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-08-12 17:19:45 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map03/8-other-items.asm
+++ disasm/data/maps/entries/map03/8-other-items.asm
@@ -2,7 +2,6 @@
 ; ASM FILE data\maps\entries\map03\8-other-items.asm :
 ; 0x97940..0x97946 : 
                 mapItem 59, 17, 128, HEALING_SEED
-				mapItem 3, 22, 132, MANA
 				mapItem 11, 10, 237, ANGEL_WING
 				mapItem 26, 13, 248, BRAVE_APPLE
                 endWord
