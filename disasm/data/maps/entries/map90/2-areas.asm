95390be3124bd2691c21784834412fffd9d53ad9
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-08-15 14:48:16 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map90/2-areas.asm
+++ disasm/data/maps/entries/map90/2-areas.asm
@@ -2,12 +2,12 @@
 
                     mainLayerStart      0, 0
                     mainLayerEnd        25, 25
-                    scndLayerFgndStart  0, 0
+                    scndLayerFgndStart  28, 0
                     scndLayerBgndStart  0, 0
                     mainLayerParallax   256, 256
                     scndLayerParallax   256, 256
                     mainLayerAutoscroll 0, 0
                     scndLayerAutoscroll 0, 0
-                    mainLayerType    255
-                    areaDefaultMusic 36
+                    mainLayerType    0
+                    areaDefaultMusic 38
                 endWord
