5180dee1586726140d8615dc1157a31f75e5b580
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-09-14 06:10:51 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map100/6-warp-events.asm
+++ disasm/data/maps/entries/map100/6-warp-events.asm
@@ -1,3 +1,8 @@
 
 
+                mWarp 255, 31
+                  warpNoScroll
+                  warpMap    67
+                  warpDest   45, 11
+                  warpFacing DOWN
                 endWord
