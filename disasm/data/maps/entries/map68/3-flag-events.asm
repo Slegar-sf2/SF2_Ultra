0474e9cdca0268fc1dd497372f215b47bd6409d1
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-09-26 10:51:18 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map68/3-flag-events.asm
+++ disasm/data/maps/entries/map68/3-flag-events.asm
@@ -1,4 +1,8 @@
 
 ; ASM FILE data\maps\entries\map68\3-flag-events.asm :
-; 0xBDA58..0xBDA5A : 
+; 0xBDA58..0xBDA5A :
+               fbcFlag 548             ;battle 48 completed Rindo circus
+                  fbcSource 0, 0
+                  fbcSize   1, 1
+                  fbcDest   55, 43 
                 endWord
