d95c9c7e6171040a59ba6f03689c606bf4ee5c8b
slegar <110447655+Slegar-sf2@users.noreply.github.com>
2023-09-27 17:25:15 +0300

    SF2_Ultra

--- disasm/data/maps/entries/map20/7-chest-items.asm
+++ disasm/data/maps/entries/map20/7-chest-items.asm
@@ -1,4 +1,5 @@
 
 ; ASM FILE data\maps\entries\map20\7-chest-items.asm :
-; 0xA5434..0xA5436 : 
+; 0xA5434..0xA5436 :
+                mapItem 36, 55, 174, BREAD
                 endWord
--- disasm/data/maps/entries/map36/8-other-items.asm
+++ disasm/data/maps/entries/map36/8-other-items.asm
@@ -1,7 +1,6 @@
 
 ; ASM FILE data\maps\entries\map36\8-other-items.asm :
 ; 0xAE080..0xAE08A : 
-                mapItem 22, 4, 174, FAIRY_POWDER
                 mapItem 30, 19, 175, PEGASUS_WING
 				mapItem 57, 5, 252, BRIGHT_HONEY
 				mapItem 13, 10, 253, SNOWFLAKE
