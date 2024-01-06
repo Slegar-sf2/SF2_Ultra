e58dbbc145a4b0053b28ca3c7ed47037bcbe2fc5
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-06 14:52:45 -0500

    Update allystats10.asm

--- disasm/data/stats/allies/stats/allystats10.asm
+++ disasm/data/stats/allies/stats/allystats10.asm
@@ -3,12 +3,16 @@
 ; 0x1EE4DC..0x1EE4FE : Ally stats 10
 AllyStats10:    forClass  BDMN ; luke
                 hpGrowth  12, 48, LINEAR
-                mpGrowth  2, 6, EARLY
+                mpGrowth  2, 35, EARLY
                 attGrowth 8, 44, LINEAR
                 defGrowth 6, 35, LINEAR
                 agiGrowth 7, 50, LINEAR
                 spellList &
                     1, HYPER_HEAL, &
+                    10,HYPER_HEAL|LV2|,&
+                   20,HYPER_HEAL|LV3|,&
+                   30,HYPER_HEAL|LV4|
+                   40,AQUA|LV2|
                     15, BLAST, &
                     22, BLAST|LV2, &                    
                     35, BLAST|LV3, &
