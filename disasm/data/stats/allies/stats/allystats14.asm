49b69f20665cf0c5db8c5c0a0635d2c8df2a6c0a
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:20:56 -0500

    Update allystats14.asm

--- disasm/data/stats/allies/stats/allystats14.asm
+++ disasm/data/stats/allies/stats/allystats14.asm
@@ -2,9 +2,9 @@
 ; ASM FILE data\stats\allies\stats\allystats14.asm :
 ; 0x1EE575..0x1EE5A8 : Ally stats 14
 AllyStats14:    forClass  MOUS ; MARILL
-                hpGrowth  50, 109, LATE
+                hpGrowth  50, 180, EARLY
                 mpGrowth  0, 0, NONE
-                attGrowth 70, 125, LINEAR
-                defGrowth 36, 73, EARLYANDLATE
-                agiGrowth 33, 49, LINEAR
+                attGrowth 70, 150, LINEAR
+                defGrowth 36, 90, EARLYANDLATE
+                agiGrowth 33, 90, LINEAR
                 spellList
