c05baf524610919b048765683e3a6dd8b0f44e88
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:31:41 -0500

    Update allystats31.asm

--- disasm/data/stats/allies/stats/allystats31.asm
+++ disasm/data/stats/allies/stats/allystats31.asm
@@ -3,7 +3,7 @@
 ; 0x1EE31C..0x1EE36B : Ally stats 31
 AllyStats31:    forClass  GRDR   ; BLEU
                 hpGrowth  26, 132, EARLY
-                mpGrowth  8, 16, LINEAR
+                mpGrowth  8, 80, LINEAR
                 attGrowth 69, 180, EARLY
                 defGrowth 46, 83, LINEAR
                 agiGrowth 55, 90, LINEAR
