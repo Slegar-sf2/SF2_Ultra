1e91eaf687cf110825aaba99ab8d1e56b74b2633
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:22:38 -0500

    Update allystats31.asm

--- disasm/data/stats/allies/stats/allystats31.asm
+++ disasm/data/stats/allies/stats/allystats31.asm
@@ -2,12 +2,12 @@
 ; ASM FILE data\stats\allies\stats\allystats01.asm :
 ; 0x1EE31C..0x1EE36B : Ally stats 31
 AllyStats31:    forClass  GRDR   ; BLEU
-                hpGrowth  26, 132, LATE
+                hpGrowth  26, 132, EARLY
                 mpGrowth  8, 16, LINEAR
-                attGrowth 69, 131, LATE
+                attGrowth 69, 180, EARLY
                 defGrowth 46, 83, LINEAR
                 agiGrowth 55, 90, LINEAR
                 spellList & 
                     2, THUNDER|LV2, &
-                    55, THUNDER|LV3
+                    20, THUNDER|LV3
                     
