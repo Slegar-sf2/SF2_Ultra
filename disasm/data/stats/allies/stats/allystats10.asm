1ce3cd234a3e7d7f61ef0ae34a163163bdee40de
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:18:32 -0500

    Update allystats10.asm

--- disasm/data/stats/allies/stats/allystats10.asm
+++ disasm/data/stats/allies/stats/allystats10.asm
@@ -2,21 +2,24 @@
 ; ASM FILE data\stats\allies\stats\allystats10.asm :
 ; 0x1EE4DC..0x1EE4FE : Ally stats 10
 AllyStats10:    forClass  BDMN ; luke
-                hpGrowth  12, 48, LINEAR
-                mpGrowth  2, 6, EARLY
-                attGrowth 8, 44, LINEAR
-                defGrowth 6, 35, LINEAR
+                hpGrowth  12, 55, LINEAR
+                mpGrowth  2, 36, EARLY
+                attGrowth 8, 54, LINEAR
+                defGrowth 6, 50, LINEAR
                 agiGrowth 7, 50, LINEAR
                 spellList &
                     1, HYPER_HEAL, &
-                    15, BLAST, &
-                    22, BLAST|LV2, &                    
-                    35, BLAST|LV3, &
-                    55, BLAST|LV4
+                    5,HYPER_HEAL|LV2,&
+                    20,HYPER_HEAL|LV3,&
+                    27,HYPER_HEAL|LV4,&
+                    2, BLAST, &
+                    10, BLAST|LV2, &                    
+                    15, BLAST|LV3, &
+                    25, BLAST|LV4
                     
                 forClass  BDBT
-                hpGrowth  48, 120, LATE
-                mpGrowth  6, 26, EARLY
+                hpGrowth  48, 150, LATE
+                mpGrowth  6, 75, EARLY
                 attGrowth 44, 84, LINEAR
                 defGrowth 35, 70, LINEAR
                 agiGrowth 50, 87, LINEAR
