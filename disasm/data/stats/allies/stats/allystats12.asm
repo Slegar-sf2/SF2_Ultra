beecb40c5f6af6db63f2d345b95535d23342dbce
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:19:44 -0500

    Update allystats12.asm

--- disasm/data/stats/allies/stats/allystats12.asm
+++ disasm/data/stats/allies/stats/allystats12.asm
@@ -2,19 +2,19 @@
 ; ASM FILE data\stats\allies\stats\allystats12.asm :
 ; 0x1EE50F..0x1EE542 : Ally stats 12
 AllyStats12:    forClass  SWOR ; DEANNA
-                hpGrowth  9, 46, LINEAR
-                mpGrowth  8, 16, EARLY
-                attGrowth 6, 47, LINEAR
+                hpGrowth  9, 56, LINEAR
+                mpGrowth  8, 36, EARLY
+                attGrowth 6, 57, LINEAR
                 defGrowth 6, 42, LINEAR
-                agiGrowth 4, 34, EARLY
+                agiGrowth 4, 44, EARLY
                 spellList &                    
                     1, THUNDER, &
-                    29, THUNDER|LV2, &
-                    49, THUNDER|LV3
+                    10, THUNDER|LV2, &
+                    25, THUNDER|LV3
                 
                 forClass  STAR
                 hpGrowth  46, 130, LINEAR
-                mpGrowth  16, 36, EARLY
+                mpGrowth  16, 80, EARLY
                 attGrowth 47, 90, LINEAR
                 defGrowth 42, 93, LINEAR
                 agiGrowth 34, 62, EARLYANDLATE
