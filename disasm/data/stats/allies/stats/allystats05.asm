d9c176ce13dc15510b4e1f990d1ddbd5ba8481e8
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-10 12:09:33 -0500

    Update allystats05.asm

--- disasm/data/stats/allies/stats/allystats05.asm
+++ disasm/data/stats/allies/stats/allystats05.asm
@@ -3,12 +3,15 @@
 ; 0x1EE426..0x1EE454 : Ally stats 05
 AllyStats05:    forClass  THIF    ; SLADE
                 hpGrowth  8, 48, LATE
-                mpGrowth  8, 0, NONE
+                mpGrowth  8, 36, EARLY
                 attGrowth 5, 45, LINEAR
-                defGrowth 8, 33, LATE
+                defGrowth 8, 50, LATE
                 agiGrowth 10, 55, LINEAR
                 spellList &
-                    1,EGRESS
+                    1,EGRESS,&
+                    7,HOLY THUNDER,&
+                    10,HOLY THUNDER|LV2,&
+                    20,HOLY THUNDER|LV3
                     
                 forClass  NINJ
                 hpGrowth  48, 127, LATE
@@ -18,7 +21,7 @@ AllyStats05:    forClass  THIF    ; SLADE
                 agiGrowth 55, 90, LATE
                 spellList &
                     30, KATON, &
-                    37, KATON|LV2, &                    
-                    44, KATON|LV3, &
-                    52, KATON|LV4
+                    35, KATON|LV2, &                    
+                    40, KATON|LV3, &
+                    45, KATON|LV4
                     
