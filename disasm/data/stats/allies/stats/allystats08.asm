344daa2790c8a2fbcb3ae4dc6c837a6a44eb1c23
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-11 09:05:41 -0500

    Update allystats08.asm

--- disasm/data/stats/allies/stats/allystats08.asm
+++ disasm/data/stats/allies/stats/allystats08.asm
@@ -3,15 +3,19 @@
 ; 0x1EE498..0x1EE4BA : Ally stats 08
 AllyStats08:    forClass  RNGR ;MAY
                 hpGrowth  9, 52, LINEAR
-                mpGrowth  0, 0, NONE
+                mpGrowth  0, 45, EARLY
                 attGrowth 8, 57, LINEAR
                 defGrowth 4, 45, LINEAR
                 agiGrowth 5, 45, LINEAR
-                spellList 
+                spellList &
+                1,BEAM,&
+                10,BEAM|LV2,&
+                25,BEAM|LV3,&
+                30,BEAM|LV4
                     
                 forClass  BWNT
-                hpGrowth  52, 133, LINEAR
-                mpGrowth  0, 0, NONE
+                hpGrowth  52, 150, LINEAR
+                mpGrowth  0,100, EARLY
                 attGrowth 47, 87, LINEAR
                 defGrowth 31, 77, LINEAR
                 agiGrowth 29, 69, LINEAR
