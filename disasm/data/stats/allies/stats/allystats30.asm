654cc46a1b5fb3fff9c9f2fe9f9a00d0d8abd2f7
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-11 09:10:34 -0500

    Update allystats30.asm

--- disasm/data/stats/allies/stats/allystats30.asm
+++ disasm/data/stats/allies/stats/allystats30.asm
@@ -3,15 +3,20 @@
 ; 0x1EE31C..0x1EE36B : Ally stats 30
 AllyStats30:    forClass  ACHH ; SHADE
                 hpGrowth  8, 54, EARLY
-                mpGrowth  0, 0, NONE
+                mpGrowth  5, 34, EARLY
                 attGrowth 6, 56, LINEAR
                 defGrowth 5, 46, EARLY
                 agiGrowth 6, 46, EARLY
-                spellList 
+                spellList &
+                1,BEAM,&
+                10,BEAM|LV2,&
+                20,BEAM|LV3,&
+                30,BEAM|LV4
+
                     
                 forClass  SNII
-                hpGrowth  44, 109, LATE
-                mpGrowth  0, 0, NONE
+                hpGrowth  44, 150, LATE
+                mpGrowth  0, 100, EARLY
                 attGrowth 46, 100, LINEAR
                 defGrowth 26, 70, LINEAR
                 agiGrowth 30, 63, LINEAR
