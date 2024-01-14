39ceea5eb9b3dd75de781e5a953b8559b73ed521
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-14 11:59:28 -0500

    Update shopinventories.asm

--- disasm/data/stats/items/shopinventories.asm
+++ disasm/data/stats/items/shopinventories.asm
@@ -9,6 +9,7 @@ tbl_ShopInventories:
                 
                 ; Weapon shop 0
                 shopInventory SHORT_SWORD, &
+                              BRONZE SWORD,&
                               SHORT_SPEAR, &
                               SHORT_AXE, &
                               WOODEN_ROD, &
