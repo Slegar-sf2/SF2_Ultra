a29e9be0987d0249cc9cd79171585224dd0b130e
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-14 12:27:58 -0500

    Update itemdefs.asm

--- disasm/data/stats/items/itemdefs.asm
+++ disasm/data/stats/items/itemdefs.asm
@@ -1674,9 +1674,9 @@ tbl_ItemDefs:
                 itemType     RING|RARE
                 useSpell     MUDDLE|LV3
                 equipEffects INCREASE_MOV,2 , &
-                             INCREASE_DEF,15 , &
-                             INCREASE_AGI,15,&
-                             REGAIN_MP,15
+                             INCREASE_DEF,5 , &
+                             INCREASE_AGI,5,&
+                             REGAIN_MP
                 
                 ; 150: EGG
                 equipFlags   PHNK|PHNX
