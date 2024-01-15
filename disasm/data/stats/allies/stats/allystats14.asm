f9292d680ad0460a9f2825f2a1d83493e986e91c
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-15 14:02:48 -0500

    Update allystats14.asm

--- disasm/data/stats/allies/stats/allystats14.asm
+++ disasm/data/stats/allies/stats/allystats14.asm
@@ -4,10 +4,13 @@
 AllyStats14:    forClass  MOUS ; MARILL
                 hpGrowth  50, 180, EARLY
                 mpGrowth  0, 0, NONE
-                attGrowth 70, 150, LINEAR
-                defGrowth 36, 90, EARLYANDLATE
-                agiGrowth 33, 90, LINEAR
+                attGrowth 70, 150, EARLY
+                defGrowth 36, 90, EARLY
+                agiGrowth 33, 90, EARLY
                 spellList &
+                2,ICY_BREATH,&
+                5,ICY_BREATH|LV2,&
+                12,ICY_BREATH|LV3,&
                 1,SNOW,&
                 15,SNOW|LV2,&
                 20,SNOW|LV3,&
