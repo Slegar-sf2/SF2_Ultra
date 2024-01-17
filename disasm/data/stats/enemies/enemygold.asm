40ee5a9179376a553da5946b9ed2e70b8e185920
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-17 15:59:17 -0500

    Update enemygold.asm

--- disasm/data/stats/enemies/enemygold.asm
+++ disasm/data/stats/enemies/enemygold.asm
@@ -1,7 +1,7 @@
 
 ; ASM FILE data\stats\enemies\enemygold.asm :
 ; 0xBECC..0xC024 : Enemy gold amounts
-tbl_EnemyGold:  dc.w 1000                ; OOZE
+tbl_EnemyGold:  dc.w 1,000                ; OOZE
                 dc.w 35                 ; HUGE RAT
                 dc.w 60                 ; GALAM SOLDIER
                 dc.w 85                 ; GALAM KNIGHT
